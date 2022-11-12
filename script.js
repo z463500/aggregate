adb963ba28bd8aa2e4e8290336032137795220d7
z463500 <913111234@qq.com>
2022-11-13 06:54:23 +0800

    Added javascript and changed HTML Structure

--- script.js
+++ script.js
@@ -1,21 +1,121 @@
 
 
-const btnHam = document.querySelector('.ham-btn');
-const btnTimes = document.querySelector('.times-btn');
-const navBar = document.getElementById('nav-bar');
+// const btnHam = document.querySelector('.ham-btn');
+// const btnTimes = document.querySelector('.times-btn');
+// const navBar = document.getElementById('nav-bar');
 
-btnHam.addEventListener('click', function(){
-    if(btnHam.className !== ""){
-        btnHam.style.display = "none";
-        btnTimes.style.display = "block";
-        navBar.classList.add("show-nav");
-    }
-})
+// btnHam.addEventListener('click', function(){
+//     if(btnHam.className !== ""){
+//         btnHam.style.display = "none";
+//         btnTimes.style.display = "block";
+//         navBar.classList.add("show-nav");
+//     }
+// })
 
-btnTimes.addEventListener('click', function(){
-    if(btnHam.className !== ""){
-        this.style.display = "none";
-        btnHam.style.display = "block";
-        navBar.classList.remove("show-nav");
+// btnTimes.addEventListener('click', function(){
+//     if(btnHam.className !== ""){
+//         this.style.display = "none";
+//         btnHam.style.display = "block";
+//         navBar.classList.remove("show-nav");
+//     }
+// })
+function triggerTest()
+{
+    var el = document.getElementById("news_head")
+    if(el)
+    {
+        el.innerHTML = formatTextELement("This is a test", "Double");
     }
-})
+}
+function triggerTest2()
+{
+    var el = document.getElementById("news_left")
+    if(el)
+    {
+        el.innerHTML = formatImageELement("This is a test", "Double","images/banner-news-1.jpg","test","topic","desc","link","cta");
+    }      
+}            
+function formatTextELement(title,author)
+{
+    var txtFormat = "<h3>[[->TITLE<-]]<span>by [[->AUTHOR<-]]</span></h3>";
+    var str = "";
+    str = txtFormat;
+    if (title != null)
+    {
+      str = splitJoin(str,"[[->TITLE<-]]",title);
+    }  
+    if (author != null)
+    {
+      str = splitJoin(str,"[[->AUTHOR<-]]",author);
+    }
+    return str;
+
+}
+
+function formatImageELement(title, author, img, alt, topic, desc, link, cta)
+{
+    var imgFormat = '<div class="hot-topic"> \
+    <img src="[[->IAMGE<-]]" alt="[[->ALT<-]]"> \
+    <div class="hot-topic-content"> \
+        <h2>[[->TITLE<-]]</h2> \
+        <h3>[[->TOPIC<-]]</h3> \
+        <p>[[->DESC<-]]</p> \
+        <a href="[[->LINK<-]]">[[->CTA<-]]</a> \
+    </div> \
+';
+
+  var str = "";
+  str = imgFormat;
+  if (title != null)
+  {
+    str = splitJoin(str,"[[->TITLE<-]]",title);
+  }  
+  if (author != null)
+  {
+    str = splitJoin(str,"[[->AUTHOR<-]]",author);
+  }
+  if (img != null)
+  {
+    str = splitJoin(str,"[[->IAMGE<-]]",img);
+  }
+  if (alt != null)
+  {
+    str = splitJoin(str,"[[->ALT<-]]",alt);
+  }
+  if (topic != null)
+  {
+    str = splitJoin(str,"[[->TOPIC<-]]",topic);
+  }
+  if (desc != null)
+  {
+    str = splitJoin(str,"[[->DESC<-]]",desc);
+  }
+  if (link != null)
+  {
+    str = splitJoin(str,"[[->LINK<-]]",link);
+  }
+  if (cta != null)
+  {
+    str = splitJoin(str,"[[->CTA<-]]",cta);
+  }
+  return str;
+}
+
+function splitJoin(str,match,out)
+{
+    var a =null;
+    if(str != null && match != null &&  out != null) 
+    {
+        if (typeof str != 'string')
+        {
+            str = String(str);
+        }
+        a = str.split(match);
+        a = a.join(out);
+    }
+    else
+    {
+        a =str;
+    }
+    return (a);
+}
